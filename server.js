bb284bef332a9b4e1656a5383841be0abbe3692e
Lylian <lylian.juarez@iteso.mx>
2026-04-18 18:18:42 -0600

    fix server crash

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const fetch = require("node-fetch");
+const path = require("path"); // 🔥 IMPORTANTE
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => 
+
+// 🔥 esto sirve los archivos (css, html, etc)
 app.use(express.static(__dirname));
-{
-  res.sendFile(__dirname + "/index.html");
+
+// 🔥 ruta principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const systemPrompt = `
